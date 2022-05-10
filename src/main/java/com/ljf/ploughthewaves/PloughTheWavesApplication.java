b668b3822acd7679ada3b7d80f3bcaef9fec4d72
李佳帆 <10077828+L3D_2y@user.noreply.gitee.com>
2022-05-10 22:53:39 +0800

    项目结构初始化

---
+++ src/main/java/com/ljf/ploughthewaves/PloughTheWavesApplication.java
@@ -0,0 +1,13 @@
+package com.ljf.ploughthewaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloughTheWavesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PloughTheWavesApplication.class, args);
+    }
+
+}
