8f1b10f940e7a705207a8bd4a39cb1189633fcfe
李佳帆 <3151342895@qq.com>
2022-06-10 11:35:35 +0800

    redis配置修改

--- src/main/java/com/ljf/ploughthewaves/infrastructure/util/RedisUtil.java
+++ src/main/java/com/ljf/ploughthewaves/infrastructure/util/RedisUtil.java
@@ -63,7 +63,7 @@ public class RedisUtil {
      */
     public boolean hasKey(String key) {
         try {
-            return redisTemplate.hasKey(key);
+            return Boolean.TRUE.equals(redisTemplate.hasKey(key));
         } catch (Exception e) {
             e.printStackTrace();
             return false;
